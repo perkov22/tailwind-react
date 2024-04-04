ab4608ddc8fdc10addbbbc6ec935305477cedbc7
Filip Perkov <perkov.fp@gmail.com>
2024-04-04 09:47:56 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -3,21 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='min-h-screen bg-[#333] text-white flex'>
+      <div className='m-auto'>
+        <h1>Naslov</h1>
+      </div>
     </div>
   );
 }
